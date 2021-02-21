aa0d1ff1f6e26a7c2d6e92f47002d93571949c11
frontendprof <https://github.com/frontendprof>
2021-02-21 23:59:34 +0500

    controlled input and submit events

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Create = () => {
+
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("");
+
+    const handleSubmit=e=>{
+        e.preventDefault();
+        const blog={title,body,author}
+
+        console.log(blog);
+    }
+
+
     return (
         <div className="create">
             <h2>Add a new blog</h2>
+
+            <form onSubmit={handleSubmit}>
+
+                <label>Blog Title</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e)=>setTitle(e.target.value)}
+                />
+
+
+                <label>Blog Body:</label>
+                <textarea 
+                    required
+                    value={body}
+                    onChange={(e)=>setBody(e.target.value)}
+                ></textarea>
+
+
+
+                <label>Blog Author:</label>
+                <select
+                    value={author}
+                    onChange={(e)=>setAuthor(e.target.value)}
+                >
+                    <option value="">Choose the author</option>
+                    <option value="abdulmalik">AbdulMalik</option>
+                    <option value="guest12">Guest12</option>
+                    <option value="viking3">Viking3</option>
+                    
+                </select>
+
+                <button>Add Blog</button>          
+
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+
+            </form>
         </div>
     )
 }
